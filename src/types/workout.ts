4bfa722c176b43ebdcc187018e986582b2f0900e
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2026-06-08 20:00:04 +0300

    Update workout.ts

--- src/types/workout.ts
+++ src/types/workout.ts
@@ -2,10 +2,13 @@ export type WorkoutDifficulty = 'Beginner' | 'Intermediate' | 'Advanced';
 
 export type Workout = {
   id: string;
+  slug?: string;
   name: string;
+  description: string;
   image: string;
   category: string;
+  level: WorkoutDifficulty;
+  difficulty: WorkoutDifficulty;
   duration: number;
   calories: number;
-  difficulty: WorkoutDifficulty;
 };
