58ee22fbb4c6bad413cba02cf2bde40a41dd1a0f
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2026-06-24 02:09:32 +0300

    Update DashboardShell.tsx

--- src/components/DashboardShell.tsx
+++ src/components/DashboardShell.tsx
@@ -157,6 +157,7 @@ export default function DashboardShell({ children }: { children: React.ReactNode
         </header>
         {children}
         <FeedbackWidget />
+        <Footer />
       </main>
 
       {confirmLogout && (
